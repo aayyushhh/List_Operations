a9f0bc248c65d9c458ce5488e7bf8d9831442a1d
Ayush.Dodake <Ayush.Dodake@DELL1558>
2024-08-01 23:49:07 +0530

    Add project files.

---
+++ CRUDonList/Program.cs
@@ -0,0 +1,41 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> myList = new List<string>();
+
+        
+        myList.Add("Apple");
+        myList.Add("Banana");
+        myList.Add("Cherry");
+
+        
+        Console.WriteLine("Initial List:");
+        foreach (string fruit in myList)
+        {
+            Console.WriteLine(fruit);
+        }
+
+        
+        myList[1] = "berry";  
+
+        
+        Console.WriteLine("\nList After Update:");
+        foreach (string fruit in myList)
+        {
+            Console.WriteLine(fruit);
+        }
+
+        
+        myList.Remove("Cherry");
+
+        
+        Console.WriteLine("\nList After Deletion:");
+        foreach (string fruit in myList)
+        {
+            Console.WriteLine(fruit);
+        }
+
+
+    }
+}
